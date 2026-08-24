046f193688a9d8ed80de585f664ddea2aa7ce36d
BelalMuhamed <basalbelal25@gmail.com>
2026-08-24 13:57:24 +0300

    move scripts to another folder

--- invetoryBackGroundServices/obj/Debug/net8.0/invetoryBackGroundServices.AssemblyInfo.cs
+++ invetoryBackGroundServices/obj/Debug/net8.0/invetoryBackGroundServices.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("invetoryBackGroundServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588ebfa3d2c4e33f654fe5f1d99deaa42423d3dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904731c7f57653cb666c3677cead6d4026fa7ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("invetoryBackGroundServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("invetoryBackGroundServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
